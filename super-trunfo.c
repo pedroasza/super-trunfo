4b9760579aaaff7e61427eef8d27a8235ccbaeb8
Pedro Aquino <85908083+pedroasza@users.noreply.github.com>
2025-09-22 01:10:32 -0300

    Update super-trunfo.c

--- super-trunfo.c
+++ super-trunfo.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    // Declaração das variáveis da Carta 1
+    // Váriaveis da primeira carta
     char estado1;
-    char codigo1[4]; // Ex: "A01"
+    char codigo1[4]; 
     char nomeCidade1[50];
     int populacao1;
     float area1;
     float pib1;
     int pontosTuristicos1;
 
-    // Declaração das variáveis da Carta 2
+    // Váriaveis da segunda carta
     char estado2;
     char codigo2[4];
     char nomeCidade2[50];
@@ -19,9 +19,9 @@ int main() {
     float pib2;
     int pontosTuristicos2;
 
-    // Entrada de dados da Carta 1
-    printf("=== Cadastro da Carta 1 ===\n");
-    printf("Digite o Estado (A-H): ");
+    // Informações da Carta 1
+    printf("Cadastro da Carta 1\n");
+    printf("Digite o Estado: ");
     scanf(" %c", &estado1);
 
     printf("Digite o Código da Carta (ex: A01): ");
@@ -43,8 +43,8 @@ int main() {
     scanf("%d", &pontosTuristicos1);
 
 
-    // Entrada de dados da Carta 2
-    printf("\n=== Cadastro da Carta 2 ===\n");
+    // Informações da Carta 2
+    printf("Cadastro da Carta 2\n");
     printf("Digite o Estado (A-H): ");
     scanf(" %c", &estado2);
 
@@ -67,7 +67,7 @@ int main() {
     scanf("%d", &pontosTuristicos2);
 
 
-    // Exibição dos dados da Carta 1
+    // Mostrar os dados da Carta 1
     printf("\n=== Carta 1 ===\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -77,7 +77,7 @@ int main() {
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
-    // Exibição dos dados da Carta 2
+    // Mostrar os dados da Carta 2
     printf("\n=== Carta 2 ===\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
